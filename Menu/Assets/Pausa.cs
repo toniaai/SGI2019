29b3b8cafd738ef222e7578f7fe3cd9d36b6ffed
Miguel Ros Amaya <mra56@alu.ua.es>
2020-01-16 16:06:22 +0100

    Añadir lo de pausa

---
+++ Menu/Assets/Pausa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool pausa;
+    Canvas pantalla;
+
+    void Start()
+    {
+        pantalla = GetComponent<Canvas>();
+        pantalla.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            pausa = !pausa;
+            pantalla.enabled = pausa;
+            Time.timeScale = (pausa) ? 0 : 1f;
+        }
+    }
+}
